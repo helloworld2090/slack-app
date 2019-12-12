992a0be2ab0f6cf9579f5bbc5545581091a629a3
Bob Ma <bob.ma77i@gmail.com>
2019-12-12 12:31:09 +1100

    email fun

---
+++ backend/auth_passwordreset_request.py
@@ -0,0 +1,38 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import hashlib
+from GLOBAL import GLOBAL_DATA , User, generate_token, generate_user_id
+
+def auth_password_reset(user_email):
+    
+    valid_email = False
+    for users in GLOBAL_DATA["users"]:
+        if user_email == users.email:
+            valid_email = True
+            break
+    
+    if valid_email == False:
+        raise ValueError("email is not in our system")
+
+    code = hashlib.sha256(user_email.encode()).hexdigest()
+    #my email deets:
+    email = 'bob.ma000@gmail.com'
+    password = 'Polik91$'
+
+    msg = MIMEMultipart()
+
+    msg['From'] = email
+    msg['To'] = user_email
+    msg['Subject'] = 'password reset'
+
+    msg.attach(MIMEText(code, "plain"))
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    text = msg.as_string()
+    server.sendmail(email, user_email, code)
+    server.quit()
+
+    return {}
---
+++ backend/auth_passwordreset_request_test.py
@@ -0,0 +1,18 @@
+from auth_login import auth_login
+from auth_passwordreset_request import auth_password_reset
+from auth_register import auth_register
+import pytest
+
+# Register a user with valid credentials and log them in
+user1 = auth_register('bob.ma77i@gmail.com', 'valid_password1', 'valid_first1', 'valid_last1')
+res1 = auth_login('bob.ma77i@gmail.com', 'valid_password1')
+
+def test_auth_passwordreset_request_valid_user():
+    # Test a valid email belonging to a registered user
+    assert auth_password_reset('bob.ma77i@gmail.com') == {}
+
+def test_auth_passwordreset_request_non_user():
+    # Test a valid email that does not belong to a registered user
+    with pytest.raises(ValueError):
+        auth_password_reset('non_user@email.com')
+
